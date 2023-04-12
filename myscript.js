f4f699238fab94324c9d742bde77171aff796381
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-04-12 14:54:59 +0300

    Fix the score counter.

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
 
 //function that receives the preference of the user and stores it at variable choice
 // playerSelection = ()=> prompt("Enter your choice."); 
-
 let computerScore = 0;
 let playerScore = 0;
+
 console.log(" This is a test for player score " + playerScore);
 
 function playerSelection() {
@@ -26,61 +26,52 @@ function getComputerChoice(){
 }
 
 
-function winner(player, computer) {
-    let display;
-    if (player === computer) {
-        display = "Draw! ";
+function playRound(playerChoice, computerChoice) {
+    
+    if (playerChoice === computerChoice) {
         playerScore++;
         computerScore++;
+        return "Draw! ";
+       
     }
-    else if (player === "rock" && computer === "scissor" || player === "scissor" && computer === "paper" || player === "paper" && computer === "rock") {
+    else if (playerChoice === "rock" && computerChoice === "scissor" || playerChoice === "scissor" && computerChoice === "paper" || playerChoice === "paper" && computerChoice === "rock") {
         playerScore++;
-        display = `You won! ${player} beats ${computer}`;
+        return `You win! ${playerChoice} beats ${computerChoice}`;
        
     }
 
     else {
         computerScore++;
-        display = `You lost! ${computer} beats ${player}`;
+        return `You lose! ${computerChoice} beats ${playerChoice}`;
         
     }
-
-
-
-    
     //console.log("Player is " + player);
     //console.log("Computer is " + computer);
-    return display;
 
-}
-function playRound(p, c) {
-    return winner(p, c); 
 }
 
 function game() {
 
     let winnerOfFive;
-    
     let player;
     let computer;
+
     for (let i = 0; i <= 4; i++) {
         player = playerSelection();
         computer = getComputerChoice();
         playRound(player, computer);
         console.log("Player is " + player + " and computer is " + computer);
-        console.log(winner(player, computer));
-        console.log (" Player = " + playerScore);
-        console.log (" Computer = " + computerScore);
+        //console.log(playRound(player, computer));
+        console.log(" " + playerScore + " " + computerScore);
     }
 
    (playerScore > computerScore)? winnerOfFive = "Player ": winnerOfFive = "Computer";
     console.log ("Computer = " + computerScore);
-    console.log (" Player: = " + playerScore);
-    alert(winnerOfFive + " is the winner!"); 
+    console.log (" Player  = " + playerScore);
+    alert(`Player = ${playerScore} & Computer = ${computerScore}. ${winnerOfFive} is the winner!`); 
 }
 
 game();
 //console.log(playRound(playerSelection(), getComputerChoice()));
 
 
-playerScore
