05137c8805bd59ddf097b280188944a96ca4d9c9
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-01 13:51:43 +0300

    Add winner of five feature The result of each round and the final result of 5 is displayed in the browser.

--- myscript.js
+++ myscript.js
@@ -3,6 +3,7 @@
 // playerSelection = ()=> prompt("Enter your choice."); 
 let computerScore = 0;
 let playerScore = 0;
+let counter = 0; // number of games played
 
 //console.log(" This is a test for player score " + playerScore);
 
@@ -25,22 +26,29 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         playerScore++;
         computerScore++;
+        console.log(playerScore);
+        console.log(computerScore);
         return "Draw! ";
        
     }
     else if (playerChoice === "rock" && computerChoice === "scissor" || playerChoice === "scissor" && computerChoice === "paper" || playerChoice === "paper" && computerChoice === "rock") {
         playerScore++;
+        console.log(playerScore);
+        console.log(computerScore);
         return `You win! ${playerChoice} beats ${computerChoice}.`;
        
     }
 
     else {
         computerScore++;
+        console.log(playerScore);
+        console.log(computerScore);
         return `You lose! ${computerChoice} beats ${playerChoice}.`;
         
     }
     //console.log("Player is " + player);
     //console.log("Computer is " + computer);
+    
 
 }
 
@@ -48,10 +56,11 @@ const bud = document.querySelector('body');
 
 const buttons = document.querySelectorAll('button');
 let playerChoice;
+let winner; //winner of 5
 buttons.forEach(element => { element.addEventListener('click', function(){
-    if(this.id == "rock"){ playerChoice = "rock"};
-    if(this.id == "paper"){ playerChoice = "paper"};
-    if(this.id == "scissor") {playerChoice = "scissor"};
+    if(this.id == "rock"){ playerChoice = "rock"; counter++};
+    if(this.id == "paper"){ playerChoice = "paper"; counter++};
+    if(this.id == "scissor") {playerChoice = "scissor"; counter++};
     console.log(`Player is: ${playerChoice}`);
     let final_result = playRound(playerChoice, getComputerChoice());
     const newElement = document.createElement('div');
@@ -59,9 +68,26 @@ buttons.forEach(element => { element.addEventListener('click', function(){
     newElement.textContent = `Computer is: ${computerChoice} 
                               Player is: ${playerChoice} 
                               ${final_result}`;
+    
     newElement.style.color = 'green';
     newElement.style.fontWeight = 'Bold'
     bud.appendChild(newElement);
+    if (counter == 5) {
+        if(playerScore > computerScore) winner = "Player Won!";
+        else if (computerScore > playerScore) winner = "Computer Won";
+        else winner = "Draw!"
+        const announce = document.createElement('p');
+        announce.textContent = `Player ${playerScore} - ${computerScore} Computer`;
+        bud.appendChild(announce);
+        const breaker = document.createElement('br');
+        bud.appendChild(breaker);
+
+        const winnerOfFive = document.createElement('p');
+        //winner.classList.add("winnerOfFive");
+        winnerOfFive.textContent = winner;
+        bud.appendChild(winnerOfFive);
+        
+    }
     
 })
 
