9ae58c241bacd50d31bf67b5851dc551ef290ddc
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-01 13:17:42 +0300

    Add message to browser

--- myscript.js
+++ myscript.js
@@ -6,10 +6,10 @@ let playerScore = 0;
 
 //console.log(" This is a test for player score " + playerScore);
 
-
+let computerChoice;
 function getComputerChoice(){
     let computerSelection = Math.floor(Math.random()*3);
-    let computerChoice;
+    
 
     if (computerSelection == 0) computerChoice = "rock";
     else if(computerSelection == 1) computerChoice =  "paper";
@@ -44,17 +44,30 @@ function playRound(playerChoice, computerChoice) {
 
 }
 
+const bud = document.querySelector('body');
 
 const buttons = document.querySelectorAll('button');
+let playerChoice;
 buttons.forEach(element => { element.addEventListener('click', function(){
-    let playerChoice;
     if(this.id == "rock"){ playerChoice = "rock"};
     if(this.id == "paper"){ playerChoice = "paper"};
     if(this.id == "scissor") {playerChoice = "scissor"};
     console.log(`Player is: ${playerChoice}`);
-   console.log(playRound(playerChoice, getComputerChoice()));
+    let final_result = playRound(playerChoice, getComputerChoice());
+    const newElement = document.createElement('div');
+    newElement.classList.add('result');
+    newElement.textContent = `Computer is: ${computerChoice} 
+                              Player is: ${playerChoice} 
+                              ${final_result}`;
+    newElement.style.color = 'green';
+    newElement.style.fontWeight = 'Bold'
+    bud.appendChild(newElement);
+    
 })
 
 });
+
+
+
 
 
